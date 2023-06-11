eab31e8101e2fb0e8d752aaf36b6bb0691e03ad7
NguyenNguyenNguyenPhong <109236721+NguyenNguyenNguyenPhong@users.noreply.github.com>
2023-06-11 23:52:45 +0700

    Complete Register Signup Security JWT

--- Source_Code/Source_To_Test_and_Learn/spring-boot-3-jwt-security-update_v1.1/src/main/java/com/alibou/security/SecurityApplication.java
+++ Source_Code/Source_To_Test_and_Learn/spring-boot-3-jwt-security-update_v1.1/src/main/java/com/alibou/security/SecurityApplication.java
@@ -2,13 +2,14 @@ package com.alibou.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-
+@CrossOrigin(origins = "http://localhost:3000")
 public class SecurityApplication {
 
 	public static void main(String[] args) {
